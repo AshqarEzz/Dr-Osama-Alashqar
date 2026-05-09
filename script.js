fa5053f8277c92128657ac7eafac68c5db14c31a
AshqarEzz <alashqarezz@gmail.com>
2026-05-09 23:10:29 +0300

    updated files to view js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function searchFunction() {
+    // Get the text typed in the search bar
     let input = document.getElementById('searchBar').value.toLowerCase();
-    let cards = document.getElementsByClassName('item');
+    // Get all the cards on the current page
+    let cards = document.getElementsByClassName('card');
 
     for (let i = 0; i < cards.length; i++) {
+        // Look at the title (h3) inside each card
         let title = cards[i].getElementsByTagName('h3')[0].innerText.toLowerCase();
+        
+        // If the title matches the search, show it. Otherwise, hide it.
         if (title.includes(input)) {
             cards[i].style.display = "";
         } else {
